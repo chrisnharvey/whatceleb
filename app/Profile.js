18c4b7c41b379b5aa09adc3590037b0767ecda8c
Unknown <chris@chrisnharvey.com>
2017-10-14 23:31:28 +0100

    Adding very basic profile for now to get it working

--- app/Profile.js
+++ app/Profile.js
@@ -17,41 +17,53 @@ import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header
 
 export default class Profile extends Component<{}> {
   render() {
-    const { navigate } = this.props.navigation;
+    const {state} = this.props.navigation;
 
     return (
-        <HeaderImageScrollView
-      maxHeight={200}
-      minHeight={MIN_HEIGHT}
-      renderHeader={() => (
-        <Image source={require('../assets/NZ.jpg')} style={styles.image} />
-      )}
-    >
-      <View style={{ height: 1000 }}>
-        <TriggeringView onHide={() => console.log('text hidden')} >
-          <Text>Scroll Me!</Text>
-        </TriggeringView>
+      <View>
+        <Text>{state.params.name}</Text>
       </View>
-    </HeaderImageScrollView>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 15,
-  },
-});
+// export default class Profile extends Component<{}> {
+//   render() {
+//     const { navigate } = this.props.navigation;
+
+//     return (
+//         <HeaderImageScrollView
+//       maxHeight={200}
+//       minHeight={MIN_HEIGHT}
+//       renderHeader={() => (
+//         <Image source={require('../assets/NZ.jpg')} style={styles.image} />
+//       )}
+//     >
+//       <View style={{ height: 1000 }}>
+//         <TriggeringView onHide={() => console.log('text hidden')} >
+//           <Text>Scroll Me!</Text>
+//         </TriggeringView>
+//       </View>
+//     </HeaderImageScrollView>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 15,
+//   },
+// });
