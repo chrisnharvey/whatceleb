908e3c803cfcdcccc20a63fe4a72994341962d5c
Unknown <chris@chrisnharvey.com>
2017-10-16 18:52:18 +0100

    Linking react-native-orientation

--- android/app/src/main/java/com/whatceleb/MainApplication.java
+++ android/app/src/main/java/com/whatceleb/MainApplication.java
@@ -3,6 +3,7 @@ package com.whatceleb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
 import com.sbugert.rnadmob.RNAdMobPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OrientationPackage(),
             new RNAdMobPackage(),
             new ReactNativeConfigPackage(),
             new RCTCameraPackage()
