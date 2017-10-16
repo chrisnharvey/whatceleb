a93f4cb58d5a15be832a3267570fe51c6c5bc091
Unknown <chris@chrisnharvey.com>
2017-10-16 18:36:32 +0100

    Linking AdMob

--- android/app/src/main/java/com/whatceleb/MainApplication.java
+++ android/app/src/main/java/com/whatceleb/MainApplication.java
@@ -3,6 +3,7 @@ package com.whatceleb;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAdMobPackage(),
             new ReactNativeConfigPackage(),
             new RCTCameraPackage()
       );
